f5a4ffe83e2c6a9afa72b892f5f41663a6f92133
Abdulqadeer2024 <s223830288@deakin.edu.au>
2024-06-04 23:00:00 +1000

    Create app.test.js

---
+++ app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('./app'); // Your Express app
+
+describe('GET /', () => {
+    it('responds with Hello World', (done) => {
+        request(app)
+            .get('/')
+            .expect('Hello World', done);
+    });
+});
