6d7971ac861a416a163c9656f29c65bb28fc81ba
Abdulqadeer2024 <s223830288@deakin.edu.au>
2024-06-06 22:50:35 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
 const app = express();
 
-// Middleware
-app.use(bodyParser.json());
-
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Schema for blog posts
-const PostSchema = new mongoose.Schema({
-    title: String,
-    content: String,
-    date: { type: Date, default: Date.now }
-});
-
-const Post = mongoose.model('Post', PostSchema);
-
-// Routes
 app.get('/', (req, res) => {
-    res.send('Welcome to the Blog!');
-});
-
-// Create a post
-app.post('/posts', async (req, res) => {
-    const newPost = new Post(req.body);
-    await newPost.save();
-    res.status(201).send('Post created');
-});
-
-// Get all posts
-app.get('/posts', async (req, res) => {
-    const posts = await Post.find();
-    res.status(200).json(posts);
+    res.send('Hello World');
 });
 
 module.exports = app;
