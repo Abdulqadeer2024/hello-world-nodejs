2efcac3a008db81b83598ffe0a26603745e790ed
Abdulqadeer2024 <s223830288@deakin.edu.au>
2024-06-06 22:50:48 +1000

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,33 +1,10 @@
 const request = require('supertest');
 const app = require('./app');
-const mongoose = require('mongoose');
 
-// Setup and teardown for MongoDB
-beforeAll(async () => {
-    await mongoose.connect('mongodb://localhost/test_blog', { useNewUrlParser: true, useUnifiedTopology: true });
-});
-
-afterAll(async () => {
-    await mongoose.connection.dropDatabase();
-    await mongoose.connection.close();
-});
-
-describe('Blog API', () => {
-    it('should create a new post', async () => {
-        const res = await request(app)
-            .post('/posts')
-            .send({
-                title: 'Test Post',
-                content: 'This is a test post'
-            })
-            .expect(201);
-
-        expect(res.text).toEqual('Post created');
-    });
-
-    it('should retrieve all posts', async () => {
-        await request(app)
-            .get('/posts')
-            .expect(200);
+describe('GET /', () => {
+    it('responds with Hello World', (done) => {
+        request(app)
+            .get('/')
+            .expect('Hello World', done);
     });
 });
